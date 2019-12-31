29ccc3126b6d583f5aec2e73e8dadfb8e4b22f44
AzureZhen <rcraft@cyberconnects2.com>
2019-12-30 21:45:53 -0500

    v1.1.0 Curios reenabled for 1.15.1!

--- src/main/java/mod/azure/azurerpgitems/AzureRPGItemsMod.java
+++ src/main/java/mod/azure/azurerpgitems/AzureRPGItemsMod.java
@@ -5,9 +5,9 @@ import org.apache.logging.log4j.Logger;
 
 import mod.azure.azurerpgitems.config.Config;
 import mod.azure.azurerpgitems.util.LootHandler;
-//import mod.azure.azurerpgitems.util.MineSlashHandler;
+import mod.azure.azurerpgitems.util.MineSlashHandler;
 import net.minecraftforge.common.MinecraftForge;
-//import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
@@ -15,8 +15,8 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
-//import top.theillusivec4.curios.api.CuriosAPI;
-//import top.theillusivec4.curios.api.imc.CurioIMCMessage;
+import top.theillusivec4.curios.api.CuriosAPI;
+import top.theillusivec4.curios.api.imc.CurioIMCMessage;
 
 @Mod(AzureRPGItemsMod.MODID)
 public class AzureRPGItemsMod {
@@ -41,11 +41,11 @@ public class AzureRPGItemsMod {
 	}
 
 	private void enqueueIMC(final InterModEnqueueEvent event) {
-		//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("necklace"));
-		//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("belt"));
-		//InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("ring").setSize(2));
+		InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("necklace"));
+		InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("belt"));
+		InterModComms.sendTo("curios", CuriosAPI.IMC.REGISTER_TYPE, () -> new CurioIMCMessage("ring").setSize(2));
 		if (Config.INSTANCE.USE_COMPATIBILITY_ON_ITEMS.get()) {
-			//MinecraftForge.EVENT_BUS.register(new MineSlashHandler());
+			MinecraftForge.EVENT_BUS.register(new MineSlashHandler());
 		}
 	}
 }
