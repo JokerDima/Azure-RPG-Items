b43b41583310d1abec82767e459d67bdd9c78f95
AzureZhen <rcraft@cyberconnects2.com>
2020-03-16 21:08:25 -0400

    Not released yet but in progress.

--- src/main/java/mod/azure/azurerpgitems/util/AzureRPGItems.java
+++ src/main/java/mod/azure/azurerpgitems/util/AzureRPGItems.java
@@ -2,7 +2,6 @@ package mod.azure.azurerpgitems.util;
 
 import mod.azure.azurerpgitems.AzureRPGItemsMod;
 import mod.azure.azurerpgitems.config.Config;
-import mod.azure.azurerpgitems.items.StaffItem;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.BowItem;
 import net.minecraft.item.Item;
@@ -22,35 +21,39 @@ public class AzureRPGItems {
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
 		for (int i = 1; i <= 43; i++)
-			event.getRegistry()
-					.register(new BowItem(new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup)
-							.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get()))
-							.setRegistryName(location("bow" + i)));
+			event.getRegistry().register(new BowItem(new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup)
+					.maxDamage(Config.SERVER.BOW_MAXDAMAGE.get())).setRegistryName(location("bow" + i)));
 		for (int i = 1; i <= 11; i++)
 			event.getRegistry()
 					.register(new SwordItem(ItemTier.DIAMOND, (20 + i) / 5, -2.4F,
-							new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup).maxDamage(Config.SERVER.SWORD_MAXDAMAGE.get()))
-									.setRegistryName(location("sword" + i + "")));
+							new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup)
+									.maxDamage(Config.SERVER.SWORD_MAXDAMAGE.get()))
+											.setRegistryName(location("sword" + i + "")));
 		for (int i = 1; i <= 9; i++)
 			event.getRegistry().registerAll(
-					ring = new Item(new Item.Properties().group(AzureRPGItemsTab.AccessoriesItemGroup).maxDamage(Config.SERVER.RING_MAXDAMAGE.get()))
-							.setRegistryName(location("ring" + i + "")),
-					new StaffItem(new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup).maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get()), 0)
-							.setRegistryName(location("staff" + i + "")));
+					ring = new Item(new Item.Properties().group(AzureRPGItemsTab.AccessoriesItemGroup)
+							.maxDamage(Config.SERVER.RING_MAXDAMAGE.get())).setRegistryName(location("ring" + i + "")),
+					new BowItem(new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup)
+							.maxDamage(Config.SERVER.STAFF_MAXDAMAGE.get()))
+									.setRegistryName(location("staff" + i + "")));
 		for (int i = 1; i <= 4; i++)
 			event.getRegistry().registerAll(
 					axe = new AxeItem(ItemTier.DIAMOND, (20 + i) / 5, -2.4F,
-							new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup).maxDamage(Config.SERVER.AXE_MAXDAMAGE.get()))
-									.setRegistryName(location("axe" + i + "")),
-					new ShieldItem(new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup).maxDamage(Config.SERVER.SHIELD_MAXDAMAGE.get()))
-							.setRegistryName(location("shield" + i + "")),
+							new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup)
+									.maxDamage(Config.SERVER.AXE_MAXDAMAGE.get()))
+											.setRegistryName(location("axe" + i + "")),
+					new ShieldItem(new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup)
+							.maxDamage(Config.SERVER.SHIELD_MAXDAMAGE.get()))
+									.setRegistryName(location("shield" + i + "")),
 					new Item(new Item.Properties().group(AzureRPGItemsTab.WeaponItemGroup).maxDamage(1000))
-							.setRegistryName(location("gem" + i + "")),
-					new Item(new Item.Properties().group(AzureRPGItemsTab.AccessoriesItemGroup).maxDamage(Config.SERVER.NECKLACE_MAXDAMAGE.get()))
-							.setRegistryName(location("necklace" + i + "")));
-		event.getRegistry()
-				.register(new Item(new Item.Properties().group(AzureRPGItemsTab.AccessoriesItemGroup).maxDamage(Config.SERVER.BELT_MAXDAMAGE.get()))
-						.setRegistryName(location("belt1")));
+							.setRegistryName(location("gem" + i + "")));
+		for (int i = 1; i <= 6; i++)
+			event.getRegistry()
+					.registerAll(new Item(new Item.Properties().group(AzureRPGItemsTab.AccessoriesItemGroup)
+							.maxDamage(Config.SERVER.NECKLACE_MAXDAMAGE.get()))
+									.setRegistryName(location("necklace" + i + "")));
+		event.getRegistry().register(new Item(new Item.Properties().group(AzureRPGItemsTab.AccessoriesItemGroup)
+				.maxDamage(Config.SERVER.BELT_MAXDAMAGE.get())).setRegistryName(location("belt1")));
 		AzureRPGItemsMod.LOGGER.info("All Items registered.");
 	}
 
